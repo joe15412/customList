b61677788047d4edcf1c04ac161c4079d3cb9e71
joe raleigh <joseph.m.raleigh@gmail.com>
2017-12-11 02:07:57 -0600

    addedtests

--- CustomListTest.cs
+++ CustomListTest.cs
@@ -39,7 +39,7 @@ namespace UnitTestProject1
         {
             //arrange
             MyList<string> list = new MyList<string>();
-            string[] testIntArray = new string[5] { "Bird", "Moose", "Pig", "llama", "Cat", };
+            string[] testIntArray = new string[5] { "Bird", "Moose", "Pig", "llama", "Yoda", };
 
             //act
 
@@ -51,7 +51,7 @@ namespace UnitTestProject1
         }
 
         [TestMethod]
-        public void AddTestLargeNumber()
+        public void Add_Test_Large_Number()
         {
             //arrange
             MyList<int> list = new MyList<int>();
@@ -61,7 +61,64 @@ namespace UnitTestProject1
             Assert.AreEqual(expectedResult, list[0]);
         }
 
+        [TestMethod]
+        public void Add_String()
+        {
+            //arrange
+            MyList<string> list = new MyList<string>();
+            string expectedResult = "Yoda";
 
+            //act
+            list.Add("Yoda");
+            list.Add("Force");
+
+
+            //assert
+            Assert.AreNotSame(expectedResult, list[0]);
+        }
+        [TestMethod]
+        public void Add_Test_Double()
+        {
+            //arrange
+            MyList<double> list = new List<double>();
+            double expectedResult = 11;
+            //act
+            list.Add(.35);
+            list.Add(10.65);
+
+
+            //assert
+            Assert.AreNotEqual(expectedResult, list[0]);
+        }
+        [TestMethod]
+        public void Count_Int_Test()
+        {
+            //arrange
+            MyList<int> list = new MyList<int>() { 1, 2, 3, 4, 5 };
+            int expectedResult = 5;
+            //act
+            int actualResult = list.Count;
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        public void Count_String_Test()
+        {
+            MyList<string> list = new MyList<string>() { "Harry", "Lloyd", "Samsonite" };
+            int expectedResult = 3;
+            //act
+            int actualResult = list.Count;
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        public void To_String_Test()
+        {
+            //arrange
+            MyList<string> list = new MyList<string>() { "Foxtrot", "Uniform", "Charlie", "Kilo" };
+            //act
+            string actualResult = list.ToString();
+            string expectedResult = "Foxtrot Uniform Charlie Kilo ";
+            //assert
+            Assert.AreEqual(expectedResult, actualResult)
     }
     }
 }
